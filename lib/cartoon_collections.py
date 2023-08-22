5e5f0fc6ae4df81e5cc0b571a482effc460e9976
Grace kungu <grayckungu2@gmail.com>
2023-08-22 19:41:59 +0300

    completed task

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,16 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for i, dwarf in enumerate(dwarves, 1):
+        print(f"{i}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [call.capitalize() + "!" for call in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    return any(len(call) > 4 for call in calls)
 
-def find_the_cheese():
-    pass
+def find_the_cheese(strings):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for string in strings:
+        if string in cheeses:
+            return string
+    return None
